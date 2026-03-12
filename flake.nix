276757cea84193a58185eaaa975c65434371e9b3
amgg <2adrian2@gmail.com>
2026-03-12 14:56:27 -0700

    remove example stuff from template & set program name

--- flake.nix
+++ flake.nix
@@ -16,46 +16,32 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         craneLib = crane.mkLib pkgs;
-        # Common arguments can be set here to avoid repeating them later
-        # Note: changes here will rebuild all dependency crates
         commonArgs = {
           src = craneLib.cleanCargoSource ./.;
           strictDeps = true;
           buildInputs =
-            [
-              # Add additional build inputs here
-            ]
+            []
             ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-              # Additional darwin specific inputs can be set here
               pkgs.libiconv
             ];
         };
-        my-crate = craneLib.buildPackage (
+        cdvia = craneLib.buildPackage (
           commonArgs
           // {
             cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-            # Additional environment variables or build phases/hooks can be set
-            # here *without* rebuilding all dependency crates
-            # MY_CUSTOM_VAR = "some value";
           }
         );
       in {
         checks = {
-          inherit my-crate;
+          inherit cdvia;
         };
-        packages.default = my-crate;
+        packages.default = cdvia;
         apps.default = flake-utils.lib.mkApp {
-          drv = my-crate;
+          drv = cdvia;
         };
         devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
           checks = self.checks.${system};
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            # pkgs.ripgrep
-          ];
+          packages = [];
         };
       }
     );
